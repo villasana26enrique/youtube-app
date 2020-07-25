f095896c167651a2cbe910a4983a39c17af6055c
Enrique Villasana <enrique.villasana26@gmail.com>
2020-07-24 21:55:31 -0400

    Reproduciendo Videos en SweetAlert2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 import { Video } from 'src/app/models/youtube.models';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -21,6 +22,24 @@ export class HomeComponent implements OnInit {
         console.log(data);
         this.videos.push( ...data );
       });
+  }
+
+  verVideo( video: Video ) {
+    Swal.fire({
+      html: `
+      <h4>${ video.title }</h4>
+      <hr>
+      <iframe
+        width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/${ video.resourceId }"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      `
+    });
+    console.log(video);
   }
 
 }
